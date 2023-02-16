87e00afc0aa160e222a22f3531c08306c4adacc0
Ethan He <42473043+ethanlhe@users.noreply.github.com>
2023-02-15 20:19:44 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Portfolio from './components/Portfolio';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
