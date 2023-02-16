ac168c3185e7d91a903630434f0d882d2408b15c
Ethan He <42473043+ethanlhe@users.noreply.github.com>
2023-02-15 23:45:19 -0800

    add navbar and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,37 @@
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-scroll";
+import logo from "./logo.png";
 import "../styles/navbar.css";
 
 const NavHeader = () => {
   return (
     <div>
       <Navbar fixed="top" expand="lg" className="navbar-header">
-        <Navbar.Brand className="nav-intro" href="#home">My Name</Navbar.Brand>
+        <Navbar.Brand className="nav-intro" href="#home">
+          <Link activeClass="active" isDynamic smooth spy to="home">
+            <img
+              style={{ width: "20%", height: "20%" }}
+              src={logo}
+              alt="ETHAN"
+            />
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link href="#about">
               <Link activeClass="active" isDynamic smooth spy to="about">
-                About
+                ABOUT
               </Link>
             </Nav.Link>
             <Nav.Link href="#projects">
               <Link activeClass="active" isDynamic smooth spy to="projects">
-                Projects
+                PROJECTS
               </Link>
             </Nav.Link>
             <Nav.Link href="#contact">
               <Link activeClass="active" isDynamic smooth spy to="contact">
-                Contact
+                CONTACT
               </Link>
             </Nav.Link>
           </Nav>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,53 +6,54 @@ import "../styles/portfolio.css";
 const Portfolio = () => {
   return (
     <div>
-        <NavHeader />
-        <div className="portfolio-container">
-            <Row id="about" className="portfolio-section about-page">
-                <Row>
-                    <Col>
-                        <h1 className="title-text">ABOUT</h1>
-                        <p>Give a brief introduction of yourself! </p>
-                    </Col>
-                    <Col>
-                        <p>Insert an image of yourself here!</p>
-                    </Col>
-                </Row>
-            </Row>
-            <Row id="projects" className="portfolio-section projects-page">
-                <Row>
-                    <h1 className="title-text">PROJECTS</h1>
-                    <p>Display some of your notable projects or classwork here! </p>
-                    <p>You can add images and descriptions below</p>
-                </Row>
-                <Row>
-                    <Col>
-                        <div className="project-container">
-                            Project 1
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="project-container">
-                            Project 2
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="project-container">
-                            Project 3
-                        </div>
-                    </Col>
-                </Row>
-                
-            </Row>
-            <Row id="contact" className="portfolio-section contact-page">
-                <Row>
-                    <h1 className="title-text">CONTACT ME</h1>
-                    <p>Insert any socials here: LinkedIn, GitHub, etc. </p>
-                    <p>You can also add your resume here!</p>
-                </Row>
-                
-            </Row>
-        </div>
+      <NavHeader />
+      <div className="portfolio-container">
+        <Row id="home" className="portfolio-section home-page">
+          <Row>
+            <Col>
+              <h1 className="title-text">ETHAN HE</h1>
+            </Col>
+          </Row>
+        </Row>
+        <Row id="about" className="portfolio-section about-page">
+          <Row>
+            <h1 className="title-text">ABOUT</h1>
+            <p className="about-content">
+              We’re Mack and Pouya - professional cake tasters, wedding criers,
+              and just outright lovers of love. In short, we’re pretty big fans
+              of marriage! Marriage is such a special commitment and your
+              wedding marks the witness and celebration of this bond. We’re
+              super stoked for this amazing next chapter of your life. We
+              wholeheartedly recommend it!
+            </p>
+          </Row>
+        </Row>
+        <Row id="projects" className="portfolio-section projects-page">
+          <Row>
+            <h1 className="title-text">PROJECTS</h1>
+            <p>Display some of your notable projects or classwork here! </p>
+            <p>You can add images and descriptions below</p>
+          </Row>
+          <Row>
+            <Col>
+              <div className="project-container">Project 1</div>
+            </Col>
+            <Col>
+              <div className="project-container">Project 2</div>
+            </Col>
+            <Col>
+              <div className="project-container">Project 3</div>
+            </Col>
+          </Row>
+        </Row>
+        <Row id="contact" className="portfolio-section contact-page">
+          <Row>
+            <h1 className="title-text">CONTACT ME</h1>
+            <p>Insert any socials here: LinkedIn, GitHub, etc. </p>
+            <p>You can also add your resume here!</p>
+          </Row>
+        </Row>
+      </div>
     </div>
   );
 };
